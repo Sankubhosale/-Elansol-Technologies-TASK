df46605b89218d3e7fbec60fd6cd6a3b7874bc3e
Sankubhosale <158502118+Sankubhosale@users.noreply.github.com>
2024-07-02 23:27:01 -0400

    App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import ProtectedPage from './components/ProtectedPage';
+import './App.css';
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/protected" component={ProtectedPage} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default App;
